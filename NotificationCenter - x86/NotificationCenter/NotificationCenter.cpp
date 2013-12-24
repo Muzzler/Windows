bcca77aaeee63a0c393a76f221d6d06ceed43a93
Mathew Kurian <bluejamesbond@gmail.com>
2013-12-24 05:09:29 -0600

    Thread safe support

--- NotificationCenter - x86/NotificationCenter/NotificationCenter.cpp
+++ NotificationCenter - x86/NotificationCenter/NotificationCenter.cpp
@@ -22,7 +22,10 @@ int WINAPI WinMain( HINSTANCE xHInstance, HINSTANCE	xPrevInstance, LPSTR xLpCmdL
 	if (FAILED(hr))	return hr;
 	
 	frame->SetName(L"Muzzler - Notification Center");				// Set the title
-	
+	frame->setBackground(0xF2, 0x34, 0x11);
+	frame->setBorder(0xFF, 0xFF, 0xFF, 0xFF, 1.0f);
+	frame->setShadow(0xFF, 0x00, 0xFF, 0x00, 50.0f);
+
 	frame->SetSize(200, 200);		// Start at left corner
 	frame->SetLocationRelativeTo(NULL);	// Center on screen
 	
@@ -35,6 +38,9 @@ int WINAPI WinMain( HINSTANCE xHInstance, HINSTANCE	xPrevInstance, LPSTR xLpCmdL
 
 	frame2.SetName(L"Muzzler - Notification Center");				// Set the title
 
+	frame2.setBackground(0x00, 0x00, 0x00);
+	frame2.setBorder(0xFF, 0xFF, 0xFF, 0xFF, 1.0f);
+	frame2.setShadow(0xFF, 0x00, 0x00, 0x00, 50.0f);
 	frame2.SetBounds(0, 0, 600, 500);		// Start at left corner	
 
 	frame2.SetVisible(true);
@@ -42,10 +48,6 @@ int WINAPI WinMain( HINSTANCE xHInstance, HINSTANCE	xPrevInstance, LPSTR xLpCmdL
 	return hr;
 }
 
-void startThread()
-{
-	frame->SetVisible(true);
-}
 void startThread()
 {
 	frame->SetVisible(true);
