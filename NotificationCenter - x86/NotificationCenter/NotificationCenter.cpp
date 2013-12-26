b5f81b723485b9bb063b19479375e303e3968536
Mathew Kurian <bluejamesbond@gmail.com>
2013-12-26 03:03:08 -0600

    Added EDT files into filter

--- NotificationCenter - x86/NotificationCenter/NotificationCenter.cpp
+++ NotificationCenter - x86/NotificationCenter/NotificationCenter.cpp
@@ -3,6 +3,12 @@
 
 #include "stdafx.h"
 #include "NotificationCenter.h"
+#include <thread>
+
+
+A2DFrame * frame;
+
+void startThread();
 
 int WINAPI WinMain( HINSTANCE xHInstance, HINSTANCE	xPrevInstance, LPSTR xLpCmdLine, int xCmdShow )
 {
@@ -10,28 +16,56 @@ int WINAPI WinMain( HINSTANCE xHInstance, HINSTANCE	xPrevInstance, LPSTR xLpCmdL
 	UNREFERENCED_PARAMETER(xHInstance);
 
 	LPCTSTR file0 = L"Assets/images/free [a] 1.jpg",
-			file1 = L"Assets/images/free [a] 1.jpg",
-			file2 = L"Assets/images/free [a] 1.jpg",
-			file3 = L"Assets/images/free [a] 1.jpg",
-			file4 = L"Assets/images/free [a] 1.jpg",
+			file1 = L"Assets/images/muzzler windows-sync textureBackground [a] 1.jpg",
+			file2 = L"Assets/images/muzzler windows-sync background [a] 1.jpg",
+			file3 = L"Assets/images/muzzler windows-sync textureBackground [a] 1.jpg",
+			file4 = L"Assets/images/muzzler windows-sync textureBackground [a] 2.jpg",
 			title = L"Muzzler - Notification Center";
 
 	HRESULT hr = S_OK;
 	
-	A2DFrame frame(xHInstance);
+	frame = new A2DFrame(xHInstance);
 
-	hr = frame.Initialize();
-	if (FAILED(hr))	return hr;
-	
-	frame.SetName(title);				// Set the title
-	frame.SetBounds(0,0, 800, 600);		// Start at left corner
-	frame.SetLocationRelativeTo(NULL);	// Center on screen
+	hr = frame->Initialize();
+	if (FAILED(hr))        return hr;
 
-	A2DPanel& root = *frame.GetRootPane(); // Reference to make syntax easier
+	frame->SetName(L"Muzzler - Notification Center");                                // Set the title
+	frame->setBackground(0xF2, 0x34, 0x11);
+	frame->setBorder(0xFF, 0xFF, 0xFF, 0xFF, 10.0f);
+	frame->setShadow(0xFF, 0x00, 0xFF, 0x00, 100.0f);
 
-	root.SetOptBackgroundImage(&file0);
-	
-	frame.SetVisible(true);
+	frame->SetLocationRelativeTo(NULL);        // Center on screen
+
+	A2DPanel& root = *frame->GetRootPane(); // Reference to make syntax easier
+
+	A2DPanel mainPanel;
+	A2DPanel subPanel;
+
+	hr = mainPanel.Initialize();
+	if (FAILED(hr))        return hr;
+
+	hr = subPanel.Initialize();
+	if (FAILED(hr))        return hr;
+
+	mainPanel.SetBounds(0, 0, 600, 700);
+	subPanel.SetBounds(50, 0, 600, 600);
+
+	root.SetOptBackgroundImage(&file3);
+	mainPanel.SetOptBackgroundImage(&file4);
+	subPanel.SetOptBackgroundImage(&file2);
+
+	root.Add(&mainPanel);
+	root.Add(&subPanel);
+
+	root.SetDoubleBuffered(true);
+	subPanel.blurred = true;
+	mainPanel.blurred = true;
+
+	frame->SetVisible(true);
 
 	return hr;
+}
+void startThread()
+{
+	frame->SetVisible(true);
 }
