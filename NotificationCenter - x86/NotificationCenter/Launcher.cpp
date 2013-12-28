c105ba952851330a2cc14c1cc7cc2efab72ff566
Mathew Kurian <bluejamesbond@gmail.com>
2013-12-28 14:51:02 -0600

    Updated filters for new properties

--- NotificationCenter - x86/NotificationCenter/Launcher.cpp
+++ NotificationCenter - x86/NotificationCenter/Launcher.cpp
@@ -1,36 +1,11 @@
 
 #include "Launcher.h"
+#include <time.h>
 
 Launcher::Launcher(HINSTANCE xHInstance)
 {
 	aFrame = new A2DFrame(xHInstance);
-
-	mainPanel.Initialize();
-	subPanel.Initialize();
-	
-	// Sleep(5000);
-
-	//  aFrame->SetVisible(false);
-
-	//  Sleep(5000);
-
-	// aFrame->SetVisible(true);
-
-	// Sleep(5000);
-
-	// aFrame->dispose();
 }
-
-// Note: This feature is not allowed due to convoluted
-// code that needs happen to make this work. In fact,
-// this feature is quite useless.
-//
-//void launcher::runonmainthread()
-//{
-//	aframe->initialize_();
-//
-//	run(0);
-//}
 
 void Launcher::runOnEventDispatchingThread()
 {
@@ -41,33 +16,37 @@ void Launcher::runOnEventDispatchingThread()
 
 void Launcher::run(int xThreadId)
 {
-	// Force the random seeds to new
-	// number.
+	clock_t tStart = clock();
+
 	srand(xThreadId);
 
 	aFrame->SetName(L"Muzzler - Notification Center");                                // Set the title
 	aFrame->setBackground(45, 45, 48);
 	aFrame->setBorder(0xFF, 0, 122, 204, 1.0f);
 	aFrame->setShadow(0xFF, 0, 122, 204, 10.0f);
-	aFrame->SetBounds(rand() % 800, rand() % 600, 800, 600);
+	aFrame->SetBounds(0, 0, 800, 600);
+	aFrame->SetLocationRelativeTo(NULL);
 
 	SYSOUT_F("%s - 0x%X\n", (xThreadId == 0 ? "Main Thread" : "Event Dispatching Thread"), xThreadId);
 
 	A2DPanel& root = *aFrame->GetRootPane(); // Reference to make syntax easier
 		
-	mainPanel.SetBounds(30, 30, 100, 100);
-	subPanel.SetBounds(130, 30, 100, 100);
+	int cX = 10;
+	int cY = 10;
 
-	// root.SetOptBackgroundImage(&file3);
-	mainPanel.SetOptBackgroundImage(&file4);
-	subPanel.SetOptBackgroundImage(&file2);
-
-	root.Add(&mainPanel);
-	root.Add(&subPanel);
-
-	root.SetDoubleBuffered(true);
-	subPanel.blurred = true;
-	mainPanel.blurred = true;
+	for (int cY = 10; cY < 1000; cY += 247)
+	{
+		for (cX = 10; cX < 1000; cX += 247)
+		{
+			A2DPanel * ap = new A2DPanel();
+			ap->Initialize();
+			ap->SetBounds(cX, cY, 237, 237);
+			ap->SetOptBackgroundImage(&file4);
+			root.Add(ap);
+		}
+	}
 
 	aFrame->SetVisible(true);
+
+	SYSOUT_F("Time taken: %.9fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 }
--- NotificationCenter - x86/NotificationCenter/Launcher.h
+++ NotificationCenter - x86/NotificationCenter/Launcher.h
@@ -21,7 +21,7 @@ private:
 		file1 = L"Assets/images/muzzler windows-sync textureBackground [a] 1.jpg",
 		file2 = L"Assets/images/muzzler windows-sync background [a] 1.jpg",
 		file3 = L"Assets/images/muzzler windows-sync textureBackground [a] 1.jpg",
-		file4 = L"Assets/images/muzzler windows-sync textureBackground [a] 2.jpg",
+		file4 = L"Assets/images/muzzler windows-sync textureLogo [a] 1.png",
 		title = L"Muzzler - Notification Center";
 
 };
