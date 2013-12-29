69257a681d0d0ec0151ede57fa371345560af3c9
Brent Enriquez <benriquez0012@gmail.com>
2013-12-29 08:03:03 -0600

    Added focus listeners/events

--- NotificationCenter - x86/NotificationCenter/Launcher.cpp
+++ NotificationCenter - x86/NotificationCenter/Launcher.cpp
@@ -48,7 +48,7 @@ void Launcher::run(int xThreadId)
 	aFrame->SetName(L"Muzzler - Notification Center");                                // Set the title
 	aFrame->setBackground(45, 45, 48);
 	aFrame->setBorder(0xFF, 0, 122, 204, 1.0f);
-	aFrame->setShadow(0xFF, 0, 122, 204, 10.0f);
+	aFrame->setShadow(0xFF, 0, 122, 204, 75.0f);
 	aFrame->SetBounds(rand() % 800, rand() % 600, 800, 600);
 
 	SYSOUT_F("%s - 0x%X\n", (xThreadId == 0 ? "Main Thread" : "Event Dispatching Thread"), xThreadId);
