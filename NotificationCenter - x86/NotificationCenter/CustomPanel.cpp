605aae18fe1463c572b94b4392de912348438a36
Mathew Kurian <bluejamesbond@gmail.com>
2014-01-14 17:01:06 -0600

    Font file and customPanel using drawString

--- NotificationCenter - x86/NotificationCenter/CustomPanel.cpp
+++ NotificationCenter - x86/NotificationCenter/CustomPanel.cpp
@@ -7,7 +7,15 @@ CustomPanel::CustomPanel() : Component() {}
 
 void CustomPanel::paintComponent()
 {
+	if (!aVisible)
+	{
+		return;
+	}
+
 	Graphics& graphics = *aGraphics;
+
+	aOptBackgroundRegion.aWidth = 330;
+	aOptBackgroundRegion.aHeight = 420;
 
 	graphics.drawString(&aPipeline, aOptBackgroundRegion);
 }
